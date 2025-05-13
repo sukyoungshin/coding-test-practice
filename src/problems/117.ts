f32f7da7c6210cae1793303dd2fd2fda3c8ccbbe
suKyoung <sukyoung.dev@gmail.com>
2025-05-13 22:01:08 +0900

    fix: 117번 문제

--- src/problems/117.ts
+++ src/problems/117.ts
@@ -10,6 +10,10 @@ export function Problem117() {
   // 2번 풀이
   const [id11, id12, id13] = [...arr[0], ...arr[1]].map((item) => item.id);
 
+  // 3번 풀이
+  const [[{ id: id121 }], [{ id: id122 }, { id: id123 }]] = arr;
+
   console.log(id1, id2, id3); // 출력결과: 1 2 3
   console.log(id11, id12, id13); // 출력결과: 1 2 3
+  console.log(id121, id122, id123); // 출력결과: 1 2 3
 }
