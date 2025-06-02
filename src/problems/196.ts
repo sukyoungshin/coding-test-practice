addacb91312e8351e1289439a7b088be903d2de7
suKyoung <sukyoung.dev@gmail.com>
2025-06-02 09:44:08 +0900

    add: 196번 문제풀이

---
+++ src/problems/196.ts
@@ -0,0 +1,16 @@
+import assert from 'assert';
+
+// 1. 배열의 각 원소를 String으로 변환하시오.
+const arr = [1, 2, 3, true];
+const ret1 = arr.map((item) => String(item));
+assert.deepStrictEqual(ret1, ['1', '2', '3', 'true']);
+
+// 2. 다음과 같이 작동하는 classNames 함수를 작성하시오.
+const classNames = (...args) => {
+  return [...args].filter((a) => a !== '').join(' ');
+};
+const ret2 = classNames('', 'a b c', 'd', '', 'e');
+assert.strictEqual(ret2, 'a b c d e');
+
+// cf. example in React
+// <div classNames=({x ? 'a b' : ''}, {'' ? 'c' : ''}, {z && 'e f'})
