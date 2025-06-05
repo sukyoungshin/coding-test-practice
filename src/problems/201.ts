4394922fb4adfa85a928905f1fae46ec8cc5ef5b
suKyoung <sukyoung.dev@gmail.com>
2025-06-05 20:28:21 +0900

    fix: 201번 문제개선

--- src/problems/201.ts
+++ src/problems/201.ts
@@ -1,9 +1,10 @@
 function flattenArray(arr) {
-  const result: number[] = [];
+  let result: number[] = [];
   arr.forEach((item) => {
     if (Array.isArray(item)) {
       // result.concat(flattenArray(item)); // ❌ 문제점: concat의 결과를 result에 할당하지 않음
-      result.push(...flattenArray(item)); // ✅ 개선 방안: concat의 결과를 result에 할당
+      // result.push(...flattenArray(item)); // ✅ 개선 방안: concat의 결과를 result에 할당
+      result = [...result, ...flattenArray(item)]; // ✅
     } else {
       result.push(item);
     }
