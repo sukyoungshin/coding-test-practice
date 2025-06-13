67f42b608e33ef41aff082954c1718d3ca67d8d3
suKyoung <sukyoung.dev@gmail.com>
2025-06-13 14:53:52 +0900

    fix: 247번 문제풀이 개선

--- src/problems/247.ts
+++ src/problems/247.ts
@@ -8,30 +8,17 @@ const hong = { id: 1, name: 'Hong', dept: 1 };
 const kim = { id: 2, name: 'Kim', dept: 2 };
 const emps = [hong, kim, { id: 3, name: 'Park', dept: 2 }, { id: 4, name: 'Choi', dept: 2 }];
 
-const deptMap = new Map();
-for (let i = 0; i < depts.length; i++) {
-  const key = i + 1;
-  const value = depts[i];
+const deptMap = new Map(depts.map((dept) => [dept.id, dept]));
+const empMap = new Map(emps.map((emp) => [emp.id, emp]));
+const empDept = new Map(
+  emps.map((emp) => {
+    Object.defineProperty(emp, 'dept', {
+      enumerable: false,
+    });
 
-  deptMap.set(key, value);
-}
-
-const empMap = new Map();
-for (let i = 0; i < emps.length; i++) {
-  const key = i + 1;
-  const value = emps[i];
-
-  empMap.set(key, value);
-}
-
-const empDept = new Map();
-emps.forEach((e) => {
-  Object.defineProperty(e, 'dept', {
-    enumerable: false,
-  });
-
-  empDept.set(e, deptMap.get(e.dept));
-});
+    return [emp, deptMap.get(emp.dept)];
+  }),
+);
 
 console.log('deptMap >> ', deptMap); // Map(2) { 1 => { id: 1, dname: '인사팀' }, 2 => { id: 2, dname: '개발팀' } }
 console.log('empMap >> ', empMap); // Map(2) { 1 => {id: 1, name: 'Hong', dept: 1}, 2 => {id: 2, name: 'Kim', dept: 2}, … }
@@ -41,7 +28,7 @@ assert.deepStrictEqual(
   emps.map(({ id, name }) => ({ id, name })),
 );
 
-console.log(empDept.get(kim).dname); // '개발팀'
+console.log(empDept.get(kim)?.dname); // '개발팀'
 console.log('empDept.get(kim) >> ', empDept.get(kim));
 assert.strictEqual(empDept.get(kim)?.dname, devTeam.dname);
 
@@ -50,5 +37,4 @@ function getEmp(empId) {
 
   return { ...rest, dept: deptMap.get(dept) };
 }
-console.log('getEmp(1) >> ', getEmp(1));
 assert.deepStrictEqual(getEmp(1), { id: 1, name: 'Hong', dept: { id: 1, dname: '인사팀' } });
