fc9bfc89415b5d0ae73268c184611f05c2034ee7
suKyoung <sukyoung.dev@gmail.com>
2025-06-21 15:31:41 +0900

    fix: 237번 문제풀이 개선

--- src/problems/midterm-exam/237.ts
+++ src/problems/midterm-exam/237.ts
@@ -1,3 +1,5 @@
+import assert from 'assert';
+
 // 다음의 지하철 노선 중에서, 출발역 ~ 도착역까지만을 반환하는 클래스를 작성하시오. (단방향만!)
 const LINE2 = [
   '신도림',
@@ -50,11 +52,11 @@ const LINE2 = [
   '을지로입구',
 ];
 
-function getSubways(args) {
+function getSubwayStations(args) {
   const [s, e] = args;
 
-  const start = LINE2.findIndex((a) => a === s);
-  const end = LINE2.findIndex((a) => a === e);
+  const start = LINE2.findIndex((line) => line === s);
+  const end = LINE2.findIndex((line) => line === e);
 
   return start < end
     ? LINE2.slice(start, end + 1)
@@ -62,42 +64,83 @@ function getSubways(args) {
 }
 
 class Subway {
-  protected list: string[];
+  protected stations: string[];
 
   constructor(...args) {
-    this.list = args ? getSubways(args) : [];
+    this.stations = args ? getSubwayStations(args) : [];
   }
 
   *[Symbol.iterator]() {
-    for (const station of this.list) {
+    for (const station of this.stations) {
       yield station;
     }
   }
 }
 
+// ================== 테스트케이스1 ==================
 const routes1 = new Subway('문래', '신림');
-console.log('🚀 ~ [...routes1] >> ', [...routes1]); // [ '문래', '대림', '구로디지털단지', '신대방', '신림' ]
-console.log('🚀 ~ [...routes1].length >> ', [...routes1].length); // 5
+assert.deepStrictEqual([...routes1].length, 5); // 5개 정류장
+assert.deepStrictEqual([...routes1], ['문래', '대림', '구로디지털단지', '신대방', '신림']);
 
 const it1 = routes1[Symbol.iterator]();
-console.log(it1.next()); // { value: '문래', done: false }
-console.log(it1.next()); // { value: '대림', done: false }
-console.log(it1.next()); // { value: '구로디지털단지', done: false }
-console.log(it1.next()); // { value: '신대방', done: false }
-console.log(it1.next()); // { value: '신림', done: false }
-console.log(it1.next()); // { value: undefined, done: true }
+['문래', '대림', '구로디지털단지', '신대방', '신림'].forEach((value, i) => {
+  assert.deepStrictEqual(it1.next(), { value, done: false });
+  console.log(i, routes1.toString());
+});
+assert.deepStrictEqual(it1.next(), { value: undefined, done: true });
 
-const routes2 = new Subway('구로디지털단지', '성수'); // 32개 정거장
-console.log('🚀 ~ routes2 >> ', [...routes2]); // ['구로디지털단지', '신대방', ..., '성수']
-console.log('🚀 ~ routes2.length >> ', [...routes2].length); // 32
+// ================== 테스트케이스2 ==================
+const routes2 = new Subway('구로디지털단지', '성수');
+assert.deepStrictEqual([...routes2].length, 32); // 32개 정류장
+assert.deepStrictEqual(
+  [...routes2],
+  [
+    '구로디지털단지',
+    '신대방',
+    '신림',
+    '봉천',
+    '서울대입구',
+    '낙성대',
+    '사당',
+    '방배',
+    '서초',
+    '교대',
+    '강남',
+    '역삼',
+    '선릉',
+    '삼성',
+    '종합운동장',
+    '신천',
+    '잠실',
+    '잠실나루',
+    '강변',
+    '구의',
+    '건대입구',
+    '뚝섬',
+    '한양대',
+    '왕십리',
+    '상왕십리',
+    '신당',
+    '동대문역사문화공원',
+    '을지로4가',
+    '을지로3가',
+    '을지로입구',
+    '신도림',
+    '성수',
+  ],
+);
+
 const it2 = routes2[Symbol.iterator]();
 while (true) {
   const x = it2.next();
-  console.log('it2 >> ', x);
+  console.log(x);
   if (x.done) break;
 }
-const route3 = new Subway('문래', '합정'); // 46개 정거장이면 통과!
-console.log('🚀 ~ route3.length >> ', [...route3].length); // 46
 
-const route4 = new Subway('신도림', '을지로입구'); // 48개 정거장이면 통과!
-console.log('🚀 ~ route4.length >> ', [...route4].length); // 48
+// ================== 테스트케이스3 ==================
+const route3 = new Subway('문래', '합정');
+assert.strictEqual([...route3].length, 46); // 46개 정거장이면 통과!
+
+// ================== 테스트케이스4 ==================
+const route4 = new Subway('신도림', '을지로입구');
+assert.strictEqual([...route4].length, 48); // 48개 정거장이면 통과!
