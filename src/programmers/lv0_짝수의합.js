c4b3de1991155aeb551885fec28b5b98a88c3eab
suKyoung <sukyoung.dev@gmail.com>
2025-09-27 13:26:57 +0900

    add: 짝수의 합

---
+++ src/programmers/lv0_짝수의합.js
@@ -0,0 +1,12 @@
+function solution(n) {
+  var answer = 0;
+
+  for (let i = 2; i <= n; i += 2) {
+    answer += i;
+  }
+
+  return answer;
+}
+
+console.log(solution(10));
+console.log(solution(4));
