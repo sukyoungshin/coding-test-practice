543ba58b1672ec73d232b25ad859ad733815b65c
suKyoung <sukyoung.dev@gmail.com>
2025-09-27 13:35:56 +0900

    add: 배열 뒤집기

---
+++ src/programmers/lv0_배열뒤집기.js
@@ -0,0 +1,7 @@
+function solution(num_list) {
+  return num_list.reverse();
+}
+
+console.log(solution([1, 2, 3, 4, 5]));
+console.log(solution([1, 1, 1, 1, 1, 2]));
+console.log(solution([1, 0, 1, 1, 1, 3, 5]));
