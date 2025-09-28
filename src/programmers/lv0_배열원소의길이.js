761825dcdff52c2404b5e69e0d91d70d0c64f3e6
suKyoung <sukyoung.dev@gmail.com>
2025-09-28 15:00:10 +0900

    add: 배열 원소의 길이

---
+++ src/programmers/lv0_배열원소의길이.js
@@ -0,0 +1,6 @@
+function solution(strlist) {
+  return strlist.map((a) => a.length);
+}
+
+console.log(solution(['We', 'are', 'the', 'world!']));
+console.log(solution(['I', 'Love', 'Programmers.']));
