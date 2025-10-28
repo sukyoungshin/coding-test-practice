8cda13579303b59323a2c96074f11ff664ec8481
suKyoung <sukyoung.dev@gmail.com>
2025-10-28 19:12:32 +0900

    add: 등수구하기

---
+++ src/inflearn_coding_test/basic/2-5_등수구하기.js
@@ -0,0 +1,15 @@
+function solution(arr) {
+  const answer = Array.from({ length: arr.length }, () => 1);
+
+  for (let i = 0; i < arr.length; i++) {
+    for (let j = 0; j < arr.length; j++) {
+      if (i === j) continue;
+      if (arr[i] < arr[j]) answer[i]++;
+    }
+  }
+
+  return answer;
+}
+
+console.log(solution([87, 89, 92, 100, 76])); // 4 3 2 1 5
+console.log(solution([95, 99, 99, 98, 100])); // 5,2,2,4,1
