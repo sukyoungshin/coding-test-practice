be0a52d65011371bfd51fe44fcdf85f85fe0a8db
SuKyoung <87718391+sukyoungshin@users.noreply.github.com>
2025-11-01 12:12:38 +0900

    20251101 (#24)
    * fix: 2-5 등수구하기 수정
    
    * add: 격자판 최대합

--- src/inflearn_coding_test/basic/2-5_등수구하기.js
+++ src/inflearn_coding_test/basic/2-5_등수구하기.js
@@ -3,7 +3,6 @@ function solution(arr) {
 
   for (let i = 0; i < arr.length; i++) {
     for (let j = 0; j < arr.length; j++) {
-      if (i === j) continue;
       if (arr[i] < arr[j]) answer[i]++;
     }
   }
